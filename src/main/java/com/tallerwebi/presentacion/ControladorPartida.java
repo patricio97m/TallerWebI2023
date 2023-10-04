b0b3622e57d6da28e8460d602dc85e560bba26c3
patricio97m <patricio.97m@gmail.com>
2023-10-04 16:14:28 -0300

    vista de partida

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorPartida.java
@@ -0,0 +1,14 @@
+package com.tallerwebi.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorPartida {
+    @RequestMapping("/partida")
+    public ModelAndView irAPartida() {
+
+        return new ModelAndView("partida");
+    }
+}
