287379f8784a3acf69e84043bbead281aad9cd83
IgnacioMoyano <nachorf78@gmail.com>
2023-11-12 15:04:54 -0300

    intento_fallido_test

--- src/test/java/repositorio/RepositorioPartidaTest.java
+++ src/test/java/repositorio/RepositorioPartidaTest.java
@@ -1,31 +1,50 @@
 package repositorio;
 
+import com.tallerwebi.dominio.Carta;
+import com.tallerwebi.dominio.Mano;
 import com.tallerwebi.dominio.Partida;
 import com.tallerwebi.infraestructura.RepositorioPartida;
 import com.tallerwebi.infraestructura.RepositorioPartidaImpl;
+import com.tallerwebi.integracion.config.HibernateTestConfig;
+import com.tallerwebi.integracion.config.SpringWebTestConfig;
+import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import javax.transaction.Transactional;
+
+import java.util.ArrayList;
 
 import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.Mockito.mock;
 
 //No tenemos SpringTest
-/*public class RepositorioPartidaTest extends SpringTest{
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {SpringWebTestConfig.class, HibernateTestConfig.class})
+
+public class RepositorioPartidaTest{
 
 
     @Autowired
     RepositorioPartida repositorioPartida;
+
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @Test
     @Transactional
     @Rollback
     public void queSePuedaGuardarUnaPartida(){
     Partida partida = new Partida();
-    Partida partidaMock = mock(new Partida());
+    Partida partidaMock = mock();
 
         repositorioPartida.guardarNuevaPartida(partida);
 
@@ -34,14 +53,56 @@ import static org.mockito.Mockito.mock;
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void puedoBuscarUnaPartidaPorId(){
 
+        Carta carta1 = new Carta();
+        carta1.setId(1L);
+        carta1.setPalo("Espada");
+        carta1.setNumero((short) 1);
+        carta1.setValorTruco((short) 1);
+        carta1.setValorEnvido((short) 1);
+
+        Carta carta2 = new Carta();
+        carta2.setId(2L);
+        carta2.setPalo("Espada");
+        carta2.setNumero((short) 2);
+        carta2.setValorTruco((short) 2);
+        carta2.setValorEnvido((short) 2);
+
+        Carta carta3 = new Carta();
+        carta3.setId(3L);
+        carta3.setPalo("Espada");
+        carta3.setNumero((short) 3);
+        carta3.setValorTruco((short) 3);
+        carta3.setValorEnvido((short) 3);
+
+        Mano manoJugador = new Mano();
+        manoJugador.setCarta(1,carta1);
+        manoJugador.setCarta(2,carta2);
+        manoJugador.setCarta(3,carta3);
+
+        Mano manoIA = new Mano();
+        manoJugador.setCarta(1,carta1);
+        manoJugador.setCarta(2,carta2);
+        manoJugador.setCarta(3,carta3);
+
         //given
+        repositorioPartida.guardarNuevaMano(manoIA);
+        repositorioPartida.guardarNuevaMano(manoJugador);
+
         Partida partida1 = new Partida();
         Partida partida2 = new Partida();
 
-        session().save(partida1);
-        session().save(partida2);
+        partida1.setManoDelJugador(manoJugador);
+        partida1.setManoDeLaIa(manoIA);
+
+        partida2.setManoDelJugador(manoJugador);
+        partida2.setManoDeLaIa(manoIA);
+
+        sessionFactory.getCurrentSession().save(partida1);
+        sessionFactory.getCurrentSession().save(partida2);
 
         //when
         Partida partidaBuscada = repositorioPartida.buscarPartidaPorId(1L);
@@ -51,4 +112,3 @@ import static org.mockito.Mockito.mock;
     }
 
 }
-*/
--- src/test/java/servicios/ServicioPartidaTest.java
+++ src/test/java/servicios/ServicioPartidaTest.java
@@ -1,11 +1,12 @@
 package servicios;
 
-import com.tallerwebi.dominio.Partida;
-import com.tallerwebi.dominio.ServicioIA;
-import com.tallerwebi.dominio.ServicioPartida;
-import com.tallerwebi.dominio.ServicioPartidaImpl;
+import com.tallerwebi.dominio.*;
 import com.tallerwebi.infraestructura.RepositorioPartida;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
@@ -38,7 +39,50 @@ public class ServicioPartidaTest {
 
     @Test
     public void sePuedeIniciarPartida(){
+       Carta carta1 = new Carta();
+       carta1.setId(1L);
+       carta1.setPalo("Espada");
+       carta1.setNumero((short) 1);
+       carta1.setValorTruco((short) 1);
+       carta1.setValorEnvido((short) 1);
 
+        Carta carta2 = new Carta();
+        carta2.setId(2L);
+        carta2.setPalo("Espada");
+        carta2.setNumero((short) 2);
+        carta2.setValorTruco((short) 2);
+        carta2.setValorEnvido((short) 2);
+
+        Carta carta3 = new Carta();
+        carta3.setId(3L);
+        carta3.setPalo("Espada");
+        carta3.setNumero((short) 3);
+        carta3.setValorTruco((short) 3);
+        carta3.setValorEnvido((short) 3);
+
+        Mano manoJugador = new Mano();
+        manoJugador.setCarta(1,carta1);
+        manoJugador.setCarta(2,carta2);
+        manoJugador.setCarta(3,carta3);
+
+        ArrayList<Carta> cartasDelJugador = new ArrayList<>();
+        cartasDelJugador.add(carta1);
+        cartasDelJugador.add(carta2);
+        cartasDelJugador.add(carta3);
+
+        ArrayList<String> nombreCartasDelJugador = new ArrayList<String>();
+        String nombre1 = carta1.getPalo() + carta1.getNumero();
+        nombreCartasDelJugador.add(nombre1);
+
+        String nombre2 = carta2.getPalo() + carta2.getNumero();
+        nombreCartasDelJugador.add(nombre2);
+
+        String nombre3 = carta3.getPalo() + carta3.getNumero();
+        nombreCartasDelJugador.add(nombre3);
+
+
+
+        when(servicioPartida.getManoDelJugador(1L)).thenReturn(nombreCartasDelJugador);
         Long idPartida = whenSeIniciaLaPartida();
         thenSeGuardaLaPartida(idPartida);
 
